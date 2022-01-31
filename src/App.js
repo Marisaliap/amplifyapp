575601d9319fa6928bd9b92d7794fcb698fdf414
Marisa Lia Pascal <87451265+Marisaliap@users.noreply.github.com>
2022-01-31 16:43:54 -0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+/* import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
+  );
+}
+
+export default withAuthenticator(App); */
+
+import React from 'react';
+import './App.css';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
